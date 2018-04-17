f9c5a16f373548a100950aaf656a0665acbec829
Michael Schmid <schmm11@bfh.ch>
2018-04-17 17:59:05 +0200

    Initial Commit

---
+++ Res/Saver.java
@@ -0,0 +1,97 @@
+package com.example.msctest.proj2_accelerationsavetest;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Saver {
+
+        private File file;
+    public Saver(){
+        if(isExternalStorageWritable()){
+            //nice go On
+            Log.e("SUCESS", " Writable");
+        }
+        else{
+            //fucked up
+            Log.e("Error", "Not Writable");
+        }
+        file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "AccelerationTest.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
+    }
+
+    public void save(long timestamp, float[] mAccelGravityData){
+
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+            OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream);
+
+            BufferedWriter fbw = new BufferedWriter(writer);
+
+            fbw.append("Timestamp: "+ timestamp + "Data:" + mAccelGravityData.toString());
+            fbw.newLine();
+            fbw.close();
+            fbw.flush();
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
+
+
+
+    public File getPublicAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e("ERROR", "Directory not created");
+        }
+        return file;
+    }
+
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
